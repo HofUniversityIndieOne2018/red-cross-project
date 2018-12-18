4165047883d73fabe980e4d516d3c18c78018ea6
Oliver Hader <oliver@typo3.org>
2018-12-18 12:17:00 +0100

    [FEATURE] Extend Deployer functionality

--- deploy.php
+++ deploy.php
@@ -24,6 +24,12 @@ add('writable_dirs', []);
 // Hosts
 inventory('.deploy/hosts.yml');
 
+// Terminal messages and symbols
+set('SymProgress', '<info>➤</info>');
+set('SymInfo', '<info>✔</info>');
+set('SymError', '<error>✘</error>');
+set('MsgClear', "\r\e[K\e[1A\r");
+
 // Tasks
 task('build', function () {
     run('cd {{release_path}} && build');
@@ -89,61 +95,91 @@ task('dotenv:load', function () {
     }
 })->desc('Read local & remote .env files');
 
-// deploy local database to host for the initial release
-task('database:deploy', function () {
+// deploy local database to remote host for the initial release
+task('database:deploy', function() {
     $releases = get('releases_list');
     if (count($releases) === 0) {
-        set('database_file', sha1(uniqid()) . '.sql');
-        writeln('<info>✔</info> No releases found, execute local database deployment');
-        writeln('<info>➤</info> Dumping local database to {{database_file}}');
-        // create database dump
-        runLocally(
+        writeln('{{SymInfo}} No releases found on remote host, pushing local database to remote host');
+        invoke('database:push');
+    } else {
+        writeln('{{SymInfo}} Release available on remote host. Skip pushing local database to remote host');
+    }
+});
+
+// push (deploy) local database to remote host (always, needs to be called explicitly)
+task('database:push', function () {
+    set('database_file', sha1(uniqid()) . '.sql');
+    writeln('{{SymProgress}} Dumping local database to {{database_file}}');
+    // create database dump
+    runLocally(
+        sprintf(
+            'mysqldump %s -c -h %s --port %s -u %s -p%s > %s',
+            escapeshellarg(get('local_db_name')),
+            escapeshellarg(get('local_db_host')),
+            escapeshellarg(get('local_db_port')),
+            escapeshellarg(get('local_db_user')),
+            escapeshellarg(get('local_db_pass')),
+            get('database_file')
+        )
+    );
+    writeln("{{MsgClear}}{{SymInfo}}");
+    // upload database dump to database directory
+    writeln('{{SymProgress}} Uploading file {{database_file}} to host');
+    run("cd {{deploy_path}} && if [ ! -d database ]; then mkdir database; fi");
+    upload('{{database_file}}', '{{deploy_path}}/database/');
+    writeln("{{MsgClear}}{{SymInfo}}");
+    // try to import database dump
+    try {
+        // @todo Optional: Create database if not existing (depends on permissons)...
+        writeln('{{SymProgress}} Importing local database {{local_db_name}} into remote database {{host_db_name}}');
+        $command = sprintf(
+            'mysql %s -h %s --port %s -u %s -p%s',
+            escapeshellarg(get('host_db_name')),
+            escapeshellarg(get('host_db_host')),
+            escapeshellarg(get('host_db_port')),
+            escapeshellarg(get('host_db_user')),
+            escapeshellarg(get('host_db_pass'))
+        );
+        run(
             sprintf(
-                'mysqldump %s -h %s --port %s -u %s -p%s > %s',
-                escapeshellarg(get('local_db_name')),
-                escapeshellarg(get('local_db_host')),
-                escapeshellarg(get('local_db_port')),
-                escapeshellarg(get('local_db_user')),
-                escapeshellarg(get('local_db_pass')),
-                get('database_file')
+                '%s < %s',
+                $command,
+                get('deploy_path') . '/database/' . get('database_file')
             )
         );
-        writeln("\r\033[K\033[1A\r<info>✔</info>");
-        // upload database dump to database directory
-        writeln('<info>➤</info> Uploading file {{database_file}} to host');
-        run("cd {{deploy_path}} && if [ ! -d database ]; then mkdir database; fi");
-        upload('{{database_file}}', '{{deploy_path}}/database/');
-        writeln("\r\033[K\033[1A\r<info>✔</info>");
-        // try to import database dump
-        try {
-            // @todo Optional: Create database if not existing (depends on permissons)...
-            writeln('<info>➤</info> Importing local database {{local_db_name}} into remote database {{host_db_name}}');
-            run(
-                sprintf(
-                    'mysql %s -h %s --port %s -u %s -p%s < %s',
-                    escapeshellarg(get('host_db_name')),
-                    escapeshellarg(get('host_db_host')),
-                    escapeshellarg(get('host_db_port')),
-                    escapeshellarg(get('host_db_user')),
-                    escapeshellarg(get('host_db_pass')),
-                    get('deploy_path') . '/database/' . get('database_file')
-                )
-            );
-            // in any case remove database dump again
-            run('cd {{deploy_path}} && if [ -e database/{{database_file}} ]; then rm database/{{database_file}}; fi');
-            writeln("\r\033[K\033[1A\r<info>✔</info>");
-        } catch (\Exception $exception) {
-            // in any case remove database dump again
-            run('cd {{deploy_path}} && if [ -e database/{{database_file}} ]; then rm database/{{database_file}}; fi');
-            writeln("\r\033[K\033[1A\r<error>✘</error>");
-            throw $exception;
+        // in any case remove database dump again
+        run('cd {{deploy_path}} && if [ -e database/{{database_file}} ]; then rm database/{{database_file}}; fi');
+        writeln("{{MsgClear}}{{SymInfo}}");
+
+        $backendUsers = has('typo3_backend_user') ? get('typo3_backend_user') : null;
+        if (is_array($backendUsers)) {
+            foreach ($backendUsers as $backendUser) {
+                if (empty($backendUser['username']) || empty($backendUser['password'])) {
+                    writeln('{{SymError}} Both "username" and "password" must be set for "typo3_backend_user" items');
+                    continue;
+                }
+                run(
+                    sprintf(
+                        '%s -e %s',
+                        $command,
+                        escapeshellarg(sprintf(
+                            'UPDATE be_users SET password=%s WHERE username=%s AND deleted=0',
+                            escapeshellarg($backendUser['password']),
+                            escapeshellarg($backendUser['username'])
+                        ))
+                    )
+                );
+                writeln('{{SymInfo}} Adjusted backend user ' . $backendUser['username']);
+            }
         }
-    } else {
-        writeln(
-            '<info>✔</info> Release available. Skipping local database deployment'
-        );
+
+    } catch (\Exception $exception) {
+        // in any case remove database dump again
+        run('cd {{deploy_path}} && if [ -e database/{{database_file}} ]; then rm database/{{database_file}}; fi');
+        writeln("{{MsgClear}}{{SymError}}");
+        throw $exception;
     }
-})->desc('Deploy local database');
+})->desc('Push local database to remote host');
 
 task('database:flush', function () {
     if (!askConfirmation('This will REMOVE contents of remote database. Continue?')) {
@@ -174,7 +210,7 @@ task('database:flush', function () {
                 escapeshellarg(sprintf('DROP TABLE %s;', $tableName))
             )
         );
-        writeln('<info>✔</info> Dropped table ' . $tableName);
+        writeln('{{SymInfo}} Dropped table ' . $tableName);
     }
 });
 
@@ -190,11 +226,11 @@ task('typo3:finish', function() {
     });
 })->desc('Finish TYPO3 environment');
 
+
 // Configuration for custom tasks
 
 after('deploy:failed', 'deploy:unlock'); // if deployment failed
 before('database:flush', 'dotenv:load'); // load .env before explicitly executing database:flush
-
 
 // List of all tasks in their according order
 // (manipulated with before() and after() for own tasks)
