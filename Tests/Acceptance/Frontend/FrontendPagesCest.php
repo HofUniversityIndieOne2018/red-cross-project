5b3d411f2d701fb611c8370eb6c0363ab0608b32
Oliver Hader <oliver@typo3.org>
2018-12-18 16:23:04 +0100

    [TASK] Add basic acceptance test

---
+++ Tests/Acceptance/Frontend/FrontendPagesCest.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types = 1);
+namespace OliverHader\RedCrossProject\Tests\Acceptance\Frontend;
+
+use OliverHader\RedCrossProject\Tests\Acceptance\Support\AcceptanceTester;
+
+class FrontendPagesCest
+{
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function homePageIsRendered(AcceptanceTester $I)
+    {
+        $I->amOnPage('/');
+        $I->see('Community', 'h2');
+        $I->see('Kids', 'h2');
+        $I->see('Activities', 'h2');
+    }
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function userLoginWorks(AcceptanceTester $I)
+    {
+        $I->wantTo('Ensure behavior of frontend user login ');
+        $I->amOnPage('/');
+        $I->click('My Red Cross', 'nav');
+
+        $I->see('User login', 'h3');
+        $I->dontSee('My Activities');
+        $I->dontSee('All Activities');
+
+        // either selecting field using XPath ...
+        $I->fillField("//input[@name='user']", 'user-001');
+        // ... or using attribute collection
+        $I->fillField(['name' => 'pass'], 'password');
+        $I->click('Login', 'form');
+
+        $I->see('My Activities');
+        $I->see('All Activities');
+    }
+}
